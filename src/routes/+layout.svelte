cb105c121ffe9f4c6e12020a2e1fcf3f84a6f05b
Facundo Martin Gordillo <me@facundogordillo.com>
2023-08-21 23:18:16 +0200

    feat: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "../app.css";
+</script>
+
+<slot />
+
+<style>
+  :global(body) {
+    color: theme('colors.gray.50');
+    background: theme('colors.blue.950');
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<main class="container mx-auto">
+  <h1>Chiro Tech</h1>
+
+  <section class="flex flex-col p-8 gap-16 md:flex-row">
+
+    <div class="flex flex-col gap-2">
+      <h1 class="text-lg border-l-2 pl-3 border-teal-600 font-bold">🔒 Innovación en Blockchain</h1>
+      <p>Nuestro equipo domina la tecnología blockchain, creando contratos inteligentes y aplicaciones descentralizadas seguras que redefinen la confianza digital.</p>
+    </div>
+
+    <div>
+        <div class="flex flex-col gap-2">
+          <h1 class="text-lg border-l-2 pl-3 border-sky-500 font-bold">🛡️ Protegiendo tus activos</h1>
+          <p>Nos especializamos en fortalecer tus activos digitales contra amenazas cambiantes, implementando protocolos de seguridad de vanguardia para mantener tus sistemas y datos a salvo.</p>
+        </div>
+    </div>
+
+    <div class="flex flex-col gap-2">
+      <h1 class="text-lg border-l-2 pl-3 border-red-600 font-bold">🚀 Formando el Futuro</h1>
+      <p>Con experiencias variadas en JavaScript, TypeScript y más, lideramos la innovación en Web3, HTMX y Svelte para llevar tus proyectos al siguiente nivel.</p>
+    </div>
+  </section>
+</main>
