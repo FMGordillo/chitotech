34dc16a7b8ff47f5f8741ce128789eb8f43e316a
Facundo Martin Gordillo <me@facundogordillo.com>
2023-09-22 11:27:44 +0200

    feat: added team and smooth link

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,3 +1,6 @@
-<a class="text-center border-2 border-black drop-shadow-sm min-w-[180px] rounded-md
-  bg-cyan-800 px-4 py-4"><slot /></a>
+<script>
+  export let onClick;
+</script>
+<a on:click={onClick} class="text-center border-2 border-black drop-shadow-sm min-w-[180px]
+  rounded-md bg-cyan-800 px-4 py-4" {...$$props}><slot /></a>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,45 @@
   const CALENDLY_URL = "https://calendly.com/chirotech/30min";
 
   let isIntersectingHeader = true;
+
+  const team = [
+    {
+      name: "Santiago Feijoó",
+      role: "Chief Tech Officer",
+      image: "team_santiago.jpeg",
+      url: "https://www.linkedin.com/in/santiago-iv%C3%A1n-feijoo-b6b72b1a4/",
+    },
+    {
+      name: "Matias Gonzalez",
+      role: "Chief Sales Officer",
+      image: "team_matias.jpeg",
+      url: "https://www.linkedin.com/in/mat%C3%ADas-gonz%C3%A1lez-6a8228155/",
+    },
+    {
+      name: "Facundo Gordillo",
+      role: "CEO & Founder",
+      image: "team_facundo.jpeg",
+      url: "https://www.linkedin.com/in/fmgordillo/",
+    },
+  ];
+
+  // All team members are equal here
+  for (let i = team.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * i);
+    let k = team[i];
+    team[i] = team[j];
+    team[j] = k;
+  }
+
+  function handleAboutUsScroll(e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+
+    target.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
 </script>
 
 <head>
@@ -54,12 +93,13 @@
       <Button href={CALENDLY_URL} target="_blank" rel="noreferrer noopener"
         >{$_("lets_talk")}</Button
       >
-      <Button>{$_("about_us")}</Button>
+      <Button href="#about_us" onClick={handleAboutUsScroll}>{$_("about_us")}</Button>
     </div>
   </div>
 </section>
 
 <h1
+  id="about_us"
   class="flex h-96 items-center justify-center
   bg-[url('/background_title-reversed.svg')] bg-left-top bg-repeat-x pt-40
   text-center text-4xl font-bold"
@@ -133,6 +173,35 @@
     class="absolute inset-0 z-0 hidden bg-[url('/background_team-reversed.svg')] bg-right bg-no-repeat lg:block"
   />
 </section>
+
+<h1
+  class="flex h-56 items-center justify-center text-center text-4xl font-bold"
+>
+  {$_("about_team")}
+</h1>
+
+<section class="container mx-auto px-8">
+  <ul class="flex flex-wrap justify-center gap-8">
+    {#each team as member}
+      <a target="_blank" rel="noreferrer noopener" href={member.url}>
+        <li class="flex flex-col items-center">
+          <img
+            src={`/${member.image}`}
+            class="mb-2 w-32 rounded-lg object-cover"
+          />
+          <h1 class="font-semibold">{member.name}</h1>
+          <h2>{member.role}</h2>
+        </li>
+      </a>
+    {/each}
+  </ul>
+</section>
+
+<h1
+  class="flex h-56 items-center justify-center text-center text-4xl font-bold"
+>
+  {$_("contact_us")}
+</h1>
 
 <style>
   @keyframes background {
