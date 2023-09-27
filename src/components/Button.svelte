bc576b98d1ae0164d14b820695461d8c9efb54d7
Santiago Feijoo <santiago.feijoo96@gmail.com>
2023-09-27 12:33:33 -0300

    hover transition al  boton

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -4,3 +4,12 @@
 <a on:click={onClick} class="text-center border-2 border-black drop-shadow-sm min-w-[180px]
   rounded-md bg-cyan-800 px-4 py-4" {...$$props}><slot /></a>
 
+  <style>
+    a{
+      /* transition: outline 0.1s linear; */
+    }
+    a:hover{
+      outline: 2px solid white;
+    }
+  </style>
+
