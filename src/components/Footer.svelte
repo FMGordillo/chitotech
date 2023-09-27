f246e9ed332d6a49a4c82ba83dbd51fb643856be
Santiago Feijoo <santiago.feijoo96@gmail.com>
2023-09-27 12:36:06 -0300

    Componentes Logo y Footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,12 @@
+<script>
+import { _ } from 'svelte-i18n'
+  import Logo from "./Logo.svelte";
+
+</script>
+
+<footer class="flex items-center justify-around h-20">
+    <Logo/>
+    <p>
+      @2023 <strong>ChiroTech</strong> - {$_("rights")}
+    </p>
+ </footer>
---
+++ src/components/Logo.svelte
@@ -0,0 +1,4 @@
+<div class="flex items-center gap-2">
+    <img class="w-10" alt="Logo" src="/logo.png" />
+    <span class="hidden font-extrabold md:block">ChiroTech</span>
+  </div>
