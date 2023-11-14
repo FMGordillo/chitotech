056a0775a13a2682fa963692a032a17d221cad87
Facundo Martin Gordillo <me@facundogordillo.com>
2023-11-14 21:10:41 +0100

    wip: added about us page

---
+++ src/routes/about-us/+page.svelte
@@ -0,0 +1,57 @@
+<script>
+  import { _ } from 'svelte-i18n';
+</script>
+<head>
+  <title>About us - Chiro Tech - Software Consultancy</title>
+  <meta
+    name="description"
+    content="Top-notch Software Consultancy in Health, Finance and Legal"
+  />
+
+  <meta property="og:title" content="Chiro Tech - Software Consultancy" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://chirotech.dev" />
+  <meta
+    property="og:image"
+    content="https://chirotech.dev/twitter_thumbnail.png"
+  />
+  <meta
+    property="og:description"
+    content="Top-notch Software Consultancy in Health, Finance and Legal"
+  />
+
+  <meta name="twitter:card" value="summary" />
+  <meta
+    name="twitter:image"
+    value="https://chirotech.dev/twitter_large_image.png"
+  />
+</head>
+
+<section class="flex h-96 flex-col items-center">
+  <h2>Our mission</h2>
+  <p>saodjfosiafdjsa</p>
+</section>
+
+<section class="flex h-96 flex-col items-center">
+  <div>
+    <h2>Our vision</h2>
+    <p>slkjfsldkjfa</p>
+  </div>
+  <div>
+    <h2>Our values</h2>
+    <p>saodjfosiafdjsa</p>
+  </div>
+</section>
+
+<section class="flex h-96 flex-col items-center">
+  <h2>Our team</h2>
+  <div class="flex gap-4 max-w-lg">
+    <div class="w-1/2">
+      <img src="/facu_ai.jpeg" alt="" />
+      <span>saofdj</span>
+    </div>
+    <div class="w-1/2">
+      <img src="/mati_ai.jpeg" alt="" />
+    </div>
+  </div>
+</section>
